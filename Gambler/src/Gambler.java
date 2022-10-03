26a83cfbdd49e0df87a892cbe30b5f0c7f4dc1b7
shreyashdj <shreyashjadhav1799@gmail.com>
2022-10-03 14:42:10 +0530

    [Shreyash]: Added code to make bet of 1 so either win or loose 1

--- Gambler/src/Gambler.java
+++ Gambler/src/Gambler.java
@@ -6,5 +6,13 @@ public class Gambler {
         System.out.println("\n ***** Welcome to GamblingSimulator *****");
         int everyDayStack = 100;
         int betForGame = 1;
+        int bet = (int) (Math.random()*2);
+        if (bet == 1) {
+            everyDayStack += betForGame;
+            System.out.println("\n Won the Bet \n everyDayStack : " + everyDayStack);
+        } else {
+            everyDayStack -= betForGame;
+            System.out.println("\n Loose the Bet \n everyDayStack : " + everyDayStack);
+        }
     }
 }
