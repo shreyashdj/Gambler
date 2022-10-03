7613ec9edc6d1db6ab14b98aa22dcf342333d7c6
shreyashdj <shreyashjadhav1799@gmail.com>
2022-10-03 18:43:51 +0530

    [Shreyash]: Added code to play for 12 month but if loss happens in current month then stop game otherwise go for next month

--- Gambler/src/Gambler.java
+++ Gambler/src/Gambler.java
@@ -10,59 +10,69 @@ public class Gambler {
     public static void main(String[] args) {
         /*
          *  printing welcome message
+         *  used for loop to play for 12 months but if loss happens stop game
          *  used for loop to play game till 20 days and every time add last day remaining stack to new day
          *  print starting stack of day
-         *  used while  loop to play game till gambler win or loose 50% of everyDayStack
+         *  used while  loop to play game till gambler win or loose 50% of Stack
          *  In while loop,
          *  make bet of 1, use Math.random to get 0 or 1
          *  if we got 1 we will add 1 to everyDayStack
          *  else we subtract 1 from everyDayStack
          *  counting number of days won and loose and by how much
+         *  printing luckiest and unluckiest day of that month
          *  print Remaining Stack
          */
         System.out.println("\n ***** Welcome to GamblingSimulator *****");
         System.out.println(" Starting stake = " + everyDayStack);
         int stack = 0;
         int startingStack;
-        int winDays = 0;
-        int looseDays = 0;
-        int winBy = 0;
-        int looseBy = 0;
-        int maxWin = 0;
-        int maxLoose = 0;
-        for (int day = 1; day <= 20; day++) {
-            stack += everyDayStack;
-            startingStack = stack;
-            int minStake = (stack-stack/2);
-            int maxStake = (stack+stack/2);
-            System.out.println("\n Day : " + day);
-            System.out.println(" Starting stake = " + stack);
-            while (stack > minStake && stack < maxStake) {
-                int makeBet = (int) (Math.random() * 2);
-                if (makeBet == 1) {
-                    stack += betForGame;
+        for (int month = 1; month <= 12; month++) {
+            int winDays = 0;
+            int looseDays = 0;
+            int winBy = 0;
+            int looseBy = 0;
+            int maxWin = 0;
+            int maxLoose = 0;
+            System.out.println("\n ***** For Month " + month + " *****");
+            for (int day = 1; day <= 20; day++) {
+                stack += everyDayStack;
+                startingStack = stack;
+                int minStake = (stack - stack / 2);
+                int maxStake = (stack + stack / 2);
+                System.out.println("\n Day : " + day);
+                System.out.println(" Starting stake = " + stack);
+                while (stack > minStake && stack < maxStake) {
+                    int makeBet = (int) (Math.random() * 2);
+                    if (makeBet == 1) {
+                        stack += betForGame;
+                    } else {
+                        stack -= betForGame;
+                    }
+                }
+                if (startingStack < stack) {
+                    if ((stack - startingStack) > winBy) {
+                        maxWin = day;
+                    }
+                    winDays += 1;
+                    winBy = (stack - startingStack);
+                    System.out.println(" Win by : " + winBy);
                 } else {
-                    stack -= betForGame;
+                    if ((startingStack - stack) > looseBy) {
+                        maxLoose = day;
+                    }
+                    looseDays += 1;
+                    looseBy = (startingStack - stack);
+                    System.out.println(" Loose by : " + looseBy);
                 }
+                System.out.println(" Remaining stake = " + stack);
             }
-            if (startingStack < stack) {
-                if ((stack - startingStack) > winBy){
-                    maxWin = day;
-                }
-                 winDays += 1;
-                 winBy = (stack - startingStack);
-                System.out.println(" Win by : " + winBy);
-            } else {
-                if ((startingStack - stack) > looseBy){
-                    maxLoose = day;
-                }
-                looseDays += 1;
-                looseBy = (startingStack - stack);
-                System.out.println(" Loose by : " + looseBy);
+            System.out.println("\n *** Month " + month + " Details ***");
+            System.out.println(" Luckiest day is Day " + maxWin + "\n Unluckiest day is Day " + maxLoose);
+            System.out.println(" Total days won : " + winDays + "\n Total days loose : " + looseDays);
+            System.out.println(" *********************");
+            if (stack < 2000) {
+                break;
             }
-            System.out.println(" Remaining stake = " + stack);
         }
-        System.out.println("\n Luckiest day is Day " + maxWin + "\n Unluckiest day is Day " + maxLoose);
-        System.out.println(" Total days won : " + winDays + "\n Total days loose : " + looseDays);
     }
 }
