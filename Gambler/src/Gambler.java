e8936662cbafef2fa2e7b449ed6651d18bfd1b20
shreyashdj <shreyashjadhav1799@gmail.com>
2022-10-03 14:16:25 +0530

    [Shreyash]: Added code to take everyDayStake of 100 and bet 1 for every game

--- Gambler/src/Gambler.java
+++ Gambler/src/Gambler.java
@@ -4,5 +4,7 @@
 public class Gambler {
     public static void main(String[] args) {
         System.out.println("\n ***** Welcome to GamblingSimulator *****");
+        int everyDayStack = 100;
+        int betForGame = 1;
     }
 }
