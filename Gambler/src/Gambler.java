77ea5e72b575ace44072cb7eedb76a0b46706260
shreyashdj <shreyashjadhav1799@gmail.com>
2022-10-03 14:07:43 +0530

    [Shreyash]: Added code to display welcome message

---
+++ Gambler/src/Gambler.java
@@ -0,0 +1,8 @@
+/**
+ * @author - Shreyash Jadhav
+ */
+public class Gambler {
+    public static void main(String[] args) {
+        System.out.println("\n ***** Welcome to GamblingSimulator *****");
+    }
+}
